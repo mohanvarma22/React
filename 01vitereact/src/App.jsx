6b22aff2f20b3317d0bfc2d9018de14f20956e07
Mohan <mohanvarma1022@gmail.com>
2024-11-04 17:48:28 +0530

    React basics

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Learn from "./learn"
+function App() {
+  
+
+  return (
+    <>
+     {/* <p>hi</p> ila unte error ochiddi because we can return only one element so wrap it in a div or empty */}
+     <p>HII</p>
+    <Learn/>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/learn.jsx
@@ -0,0 +1,7 @@
+function Learn(){
+    return(
+        <h3>Hi Guys</h3>
+    )
+}
+
+export default Learn
